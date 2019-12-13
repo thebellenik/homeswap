547aa19194cd2e0cf7bd6cf3d009c7d46655d179
Filip Kanak <kanakfil@fit.cvut.cz>
2019-12-13 12:58:21 +0200

    logo

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Home Swap</title>
+    <title>Homeswapr</title>
     <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
     <div class="content">
     <h1>Sick of commuting?</h1>
     <img src="commute.jpg">
-  	<h1>Try Home Swap!</h1>
+  	<h1>Try <img src="logo.jpg"></h1>
     <a href="/">homeswap.herokuapp.com</a>
   </div>
 </body>
